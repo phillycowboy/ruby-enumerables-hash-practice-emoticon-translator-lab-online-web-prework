deca69df2f920e59b99a1047bebc1d259a86cc37
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-28 18:25:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ end
 
 def get_english_meaning(string, emoticon)
 result=load_library(string)
-emoticon.each do |key, value|
+  result.each do |key, value|
   if result[key]={:english=>value[0]}
     result
     binding.pry
