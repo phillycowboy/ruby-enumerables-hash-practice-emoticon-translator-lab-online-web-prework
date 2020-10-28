0fd4a4f01fec3c158b5bd3be038ea31a65bb7f96
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-28 19:28:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,6 +12,15 @@ end
 
 def get_japanese_emoticon(string, emoticon)
 result=load_library(string)
+  emoticon=result.keys.find do |name|
+   result[name][:english]==emoticon
+
+  end
+  if emoticon==nil
+   "Sorry, that emoticon was not found"
+  else
+    emoticon
+  end
 end
 
 def get_english_meaning(string, emoticon)
